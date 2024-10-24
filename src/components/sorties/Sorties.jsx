e503dfe5a8b0adbd75bc5ab029a6be216545ae5c
Guillaume Sere <guillaumesere@hotmail.fr>
2024-10-24 15:49:54 +0200

    modif page sortie

--- src/components/sorties/Sorties.jsx
+++ src/components/sorties/Sorties.jsx
@@ -14,19 +14,19 @@ const Sorties = () => {
       <section className="lieux-populaires">
         <div className="lieux-grid">
         <div className="lieu">
-            <h3>Speed Park</h3>
+            <h3><a href="https://speedpark.fr/beauvais/" target="_blank" rel="noopener noreferrer">Speed Park</a></h3>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/pYrhjuSrGCk?si=8KcbEV1rtDctkweX&amp;clip=UgkxjV253z_VbfzK1iaooE_taN9vE7Vl_3w0&amp;clipt=EOaFARj9-gE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
           <div className="lieu">
-            <h3>Le Théâtre du Beauvaisis</h3>
+            <h3><a href="https://culture.beauvais.fr/acteur-culturel/theatre-du-beauvaisis" target="_blank" rel="noopener noreferrer">Le Théâtre du Beauvaisis</a></h3>
             <img src={theatre} alt="Le Théâtre du Beauvaisis" />
           </div>
           <div className="lieu">
-            <h3>Cinéma CGR</h3>
+            <h3><a href="https://www.cgrcinemas.fr/beauvais/" target="_blank" rel="noopener noreferrer">Cinéma CGR</a></h3>
             <img src={cinema} alt="Cinéma CGR" />
           </div>
           <div className="lieu">
-            <h3>Parc Marcel Dassault</h3>
+            <h3><a href="https://www.visitbeauvais.fr/fiche/parc-marcel-dassault/" target="_blank" rel="noopener noreferrer">Parc Marcel Dassault</a></h3>
             <img src={park} alt="Parc Marcel Dassault" />
           </div>
         </div>
