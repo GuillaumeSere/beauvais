b6f8074ac5fbb3fa8aee64076bf8b1139de14164
Guillaume Sere <guillaumesere@hotmail.fr>
2025-03-12 14:34:52 +0100

    refactoring

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import './country.css';
-import img1 from '../../images/carrousel1.png';
-import img2 from '../../images/carrousel17.png';
-import img3 from '../../images/carrousel3.png';
 import img4 from '../../images/banner.png';
-import img5 from '../../images/place-jeanneHachette.png';
-import img6 from '../../images/elispace.png';
-import img7 from '../../images/plan-eau.png';
+import { data } from '../../data/data.js';
 import ScrollToTopButton from '../scrollToTopButton/ScollToTopButton';
 
 const Country = () => {
@@ -20,90 +15,12 @@ const Country = () => {
             </div>
             <section className='country-section'>
                 <div className='box-container'>
-                    <div className="box">
-                        <img src={img1} alt="horloge de la cathédrale de Beauvais" />
-                        <p>
-                            Beauvais est une commune française, ville-préfecture du département
-                            de l'Oise au sein de la région culturelle et historique de Picardie,
-                            et administrative Hauts-de-France.Elle se situe sur le plateau picard,
-                            au nord du Bassin parisien et sur les rives du Thérain, affluent de l'Oise.
-                            Ses habitants sont appelés les Beauvaisiens.Elle a donné son nom au pays
-                            traditionnel du Beauvaisis, qui est historiquement un pays de Picardie dont
-                            elle est la ville principale.Sous l'Ancien Régime, l'évêque-comte
-                            de Beauvais figurait parmi les Douze pairs primitifs de France.
-                        </p>
-                    </div>
-                    <div className="box">
-                        <img src={img2} alt="mairie de Beauvais" />
-                        <p>
-                            La fusion de communes menée à la fin de la Seconde Guerre mondiale,
-                            qui a permis une reconstruction et une urbanisation des plateaux,
-                            a conforté Beauvais dans sa dynamique démographique positive.
-                            Ainsi, au dernier recensement de population, en 2021, Beauvais
-                            était la commune la plus peuplée du département de l'Oise -
-                            et la huitième au niveau régional, comptant 55 906 habitants.
-                            L'unité urbaine dont elle est la ville-centre, l'unité urbaine
-                            de Beauvais, est la troisième du département en nombre
-                            d'habitants, après celles de Creil et de Compiègne.
-                        </p>
-                    </div>
-                    <div className="box">
-                        <img src={img3} alt="rue piétonne de Beauvais" />
-                        <p>
-                            Beauvais est célèbre pour sa cathédrale d'architecture gothique,
-                            encore inachevée à ce jour puisqu'elle ne possède pas de nef,
-                            mais au chœur gothique (ou art français) le plus haut du monde,
-                            typique de l'apogée de cet art en France. La plupart des œuvres
-                            de la cathédrale datent du XVIIe siècle.
-                            Depuis 2001, les exécutifs qui se sont succédé à
-                            la tête de la ville de Beauvais ont fait de la dynamisation
-                            du centre-ville une priorité via plusieurs projets d'envergure.
-                        </p>
-                    </div>
-                    <div className="box">
-                        <img src={img5} alt="La Place Jeanne Hachette de Beauvais" />
-                        <p>
-                            La Place Jeanne Hachette est l'une des places centrales
-                            et emblématiques de Beauvais. Située en plein cœur de
-                            la ville, elle porte le nom de Jeanne Hachette, héroïne
-                            locale qui s’est illustrée en 1472 en défendant Beauvais
-                            contre les assaillants bourguignons. Cette vaste place
-                            est un lieu de rencontre et de rassemblement, bordée par
-                            des bâtiments historiques et des commerces, offrant ainsi
-                            un cadre animé et vivant. Elle accueille également de
-                            nombreux événements tout au long de l'année, comme les marchés,
-                            fêtes et manifestations culturelles, en faisant un véritable centre
-                            névralgique de la vie beauvaisienne. Au centre de la place
-                            trône une statue de Jeanne Hachette, rappelant son courage
-                            et son importance dans l’histoire de la ville.
-                        </p>
-                    </div>
-                    <div className="box">
-                        <img src={img6} alt="L'elispace de Beauvais" />
-                        <p>
-                            Outil polyvalent, l’Elispace, c’est l’accueil des sports
-                            collectifs de haut niveau (basket-ball, hand-ball, volley-ball, boxe, judo…).
-                            C’est l’une des étapes incontournables des tournées des plus grands artistes
-                            (concerts, one-man-show, théâtre, danse...). C’est un espace modulable et adaptable
-                            à tous les besoins pour l’organisation de salons, conventions et repas d’entreprises
-                        </p>
-                    </div>
-                    <div className="box">
-                        <img src={img7} alt="Le Plan d'Eau du Canada de Beauvais" />
-                        <p>
-                            Le Plan d'Eau du Canada est un vaste espace naturel et de loisirs situé
-                            à Beauvais, offrant une large gamme d'activités en plein air. Cet étang
-                            artificiel, entouré de verdure, est un lieu prisé des habitants et des
-                            visiteurs pour se détendre et profiter de la nature. Les activités
-                            proposées incluent la baignade surveillée en été, la pêche,
-                            le canoë-kayak, le pédalo, et des sentiers pour la marche et le vélo.
-                            Avec ses aires de pique-nique, ses jeux pour enfants, et ses zones
-                            de détente, le Plan d'Eau du Canada est idéal pour les familles,
-                            les sportifs, ou ceux cherchant simplement un moment de calme au cœur
-                            de la nature Beauvaisienne. Il représente un véritable poumon vert
-                            pour la ville, combinant loisirs aquatiques et découvertes environnementales.
-                        </p>
-                    </div>
+                    {data.map(item => (
+                        <div className="box" key={item.id}>
+                            <img src={item.image} alt={`Image de Beauvais ${item.id}`} />
+                            <p>{item.description}</p>
+                        </div>
+                    ))}
                 </div>
                 <ScrollToTopButton />
             </section>
--- src/data/data.js
+++ src/data/data.js
@@ -1,103 +1,41 @@
-import IMG1 from '../images/carrousel1.png'
-import IMG2 from '../images/carrousel2.png'
-import IMG3 from '../images/carrousel3.png'
-import IMG4 from '../images/carrousel4.png'
-import IMG5 from '../images/carrousel5.png'
-import IMG6 from '../images/carrousel6.png'
-import IMG7 from '../images/carrousel7.png'
-import IMG8 from '../images/carrousel8.png'
-import IMG9 from '../images/carrousel9.png'
-import IMG10 from '../images/carrousel10.png'
-import IMG11 from '../images/carrousel11.png'
-import IMG12 from '../images/carrousel12.png'
-import IMG13 from '../images/carrousel13.png'
-import IMG14 from '../images/carrousel14.png'
-import IMG15 from '../images/carrousel15.png'
-import IMG16 from '../images/carrousel16.png'
-import IMG17 from '../images/carrousel17.png'
-import IMG18 from '../images/carrousel18.png'
-import IMG19 from '../images/carrousel19.png'
-import IMG20 from '../images/carrousel20.png'
+import IMG1 from '../images/carrousel1.png';
+import IMG2 from '../images/carrousel17.png';
+import IMG3 from '../images/carrousel3.png';
+import IMG4 from '../images/place-jeanneHachette.png';
+import IMG5 from '../images/elispace.png';
+import IMG6 from '../images/plan-eau.png';
+
+
 
 export const data = [
     {
         id: 1,
         image: IMG1,
+        description: "Beauvais est une commune française, ville-préfecture du département de l'Oise au sein de la région culturelle et historique de Picardie, et administrative Hauts-de-France.Elle se situe sur le plateau picard, au nord du Bassin parisien et sur les rives du Thérain, affluent de l'Oise. Ses habitants sont appelés les Beauvaisiens.Elle a donné son nom au pays traditionnel du Beauvaisis, qui est historiquement un pays de Picardie dont elle est la ville principale.Sous l'Ancien Régime, l'évêque-comte de Beauvais figurait parmi les Douze pairs primitifs de France."
     },
     {
         id: 2,
         image: IMG2,
+        description: "La fusion de communes menée à la fin de la Seconde Guerre mondiale, qui a permis une reconstruction et une urbanisation des plateaux, a conforté Beauvais dans sa dynamique démographique positive. Ainsi, au dernier recensement de population, en 2021, Beauvais était la commune la plus peuplée du département de l'Oise - et la huitième au niveau régional, comptant 55 906 habitants. L'unité urbaine dont elle est la ville-centre, l'unité urbaine de Beauvais, est la troisième du département en nombre d'habitants, après celles de Creil et de Compiègne."
     },
     {
         id: 3,
         image: IMG3,
+        description: "Beauvais est célèbre pour sa cathédrale d'architecture gothique, encore inachevée à ce jour puisqu'elle ne possède pas de nef, mais au chœur gothique (ou art français) le plus haut du monde, typique de l'apogée de cet art en France. La plupart des œuvres de la cathédrale datent du XVIIe siècle. Depuis 2001, les exécutifs qui se sont succédé à la tête de la ville de Beauvais ont fait de la dynamisation du centre-ville une priorité via plusieurs projets d'envergure."
     },
     {
         id: 4,
         image: IMG4,
+        description: "La Place Jeanne Hachette est l'une des places centrales et emblématiques de Beauvais. Située en plein cœur de la ville, elle porte le nom de Jeanne Hachette, héroïne locale qui s’est illustrée en 1472 en défendant Beauvais contre les assaillants bourguignons. Cette vaste place est un lieu de rencontre et de rassemblement, bordée par des bâtiments historiques et des commerces, offrant ainsi un cadre animé et vivant. Elle accueille également de nombreux événements tout au long de l'année, comme les marchés, fêtes et manifestations culturelles, en faisant un véritable centre névralgique de la vie beauvaisienne. Au centre de la place trône une statue de Jeanne Hachette, rappelant son courage et son importance dans l’histoire de la ville."
     },
     {
         id: 5,
         image: IMG5,
+        description: "Outil polyvalent, l’Elispace, c’est l’accueil des sports collectifs de haut niveau (basket-ball, hand-ball, volley-ball, boxe, judo…). C’est l’une des étapes incontournables des tournées des plus grands artistes (concerts, one-man-show, théâtre, danse...). C’est un espace modulable et adaptable à tous les besoins pour l’organisation de salons, conventions et repas d’entreprises"
     },
     {
         id: 6,
         image: IMG6,
-    },
-    {
-        id: 7,
-        image: IMG7,
-    },
-    {
-        id: 8,
-        image: IMG8,
-    },
-    {
-        id: 9,
-        image: IMG9,
-    },
-    {
-        id: 10,
-        image: IMG10,
-    },
-    {
-        id: 11,
-        image: IMG11,
-    },
-    {
-        id: 12,
-        image: IMG12,
-    },
-    {
-        id: 13,
-        image: IMG13,
-    },
-    {
-        id: 14,
-        image: IMG14,
-    },
-    {
-        id: 15,
-        image: IMG15,
-    },
-    {
-        id: 16,
-        image: IMG16,
-    },
-    {
-        id: 17,
-        image: IMG17,
-    },
-    {
-        id: 18,
-        image: IMG18,
-    },
-    {
-        id: 19,
-        image: IMG19,
-    },
-    {
-        id: 20,
-        image: IMG20,
+        description: "Le Plan d'Eau du Canada est un vaste espace naturel et de loisirs situé à Beauvais, offrant une large gamme d'activités en plein air. Cet étang artificiel, entouré de verdure, est un lieu prisé des habitants et des visiteurs pour se détendre et profiter de la nature. Les activités proposées incluent la baignade surveillée en été, la pêche, le canoë-kayak, le pédalo, et des sentiers pour la marche et le vélo. Avec ses aires de pique-nique, ses jeux pour enfants, et ses zones de détente, le Plan d'Eau du Canada est idéal pour les familles, les sportifs, ou ceux cherchant simplement un moment de calme au cœur de la nature Beauvaisienne. Il représente un véritable poumon vert pour la ville, combinant loisirs aquatiques et découvertes environnementales."
     },
 ]       
