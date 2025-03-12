db2db48ee0ddc1f4d63b48730ad2ddfe5e544764
Guillaume Sere <guillaumesere@hotmail.fr>
2025-03-12 15:14:11 +0100

    add images tourisme page

--- src/components/tourisme/Tourisme.jsx
+++ src/components/tourisme/Tourisme.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './tourisme.css';
+import img1 from "../../images/cathedrale.png";
+import img2 from "../../images/musee.png";
+import img3 from "../../images/tour1.png";
+import img4 from "../../images/maladrerie.png";
 
 const Tourisme = () => {
     return (
@@ -9,11 +13,15 @@ const Tourisme = () => {
             <p>Découvrez les attractions touristiques de notre belle ville.</p>
             <div className="glass-card">
                 <ul>
-                    <li><Link to="/cathedrale">La Cathédrale Saint-Pierre</Link></li>
-                    <li><Link to="/musee">Le Musée départemental de l'Oise</Link></li>
-                    <li><Link to="/plan-eau">Le Plan d'eau du Canada</Link></li>
-                    <li><Link to="/maladrerie">La Maladrerie Saint-Lazare</Link></li>
+                    <li><Link to="/cathedrale" onMouseEnter={() => document.getElementById('cathedraleImage').style.display = 'block'} onMouseLeave={() => document.getElementById('cathedraleImage').style.display = 'none'}>La Cathédrale Saint-Pierre</Link></li>
+                    <li><Link to="/musee" onMouseEnter={() => document.getElementById('museeImage').style.display = 'block'} onMouseLeave={() => document.getElementById('museeImage').style.display = 'none'}>Le Musée départemental de l'Oise</Link></li>
+                    <li><Link to="/plan-eau" onMouseEnter={() => document.getElementById('planEauImage').style.display = 'block'} onMouseLeave={() => document.getElementById('planEauImage').style.display = 'none'}>Le Plan d'eau du Canada</Link></li>
+                    <li><Link to="/maladrerie" onMouseEnter={() => document.getElementById('maladrerieImage').style.display = 'block'} onMouseLeave={() => document.getElementById('maladrerieImage').style.display = 'none'}>La Maladrerie Saint-Lazare</Link></li>
                 </ul>
+                <div id="cathedraleImage" style={{ display: 'none' }}><img className='image' src={img1} alt="Cathédrale Saint-Pierre" /></div>
+                <div id="museeImage" style={{ display: 'none' }}><img className='image' src={img2} alt="Musée départemental de l'Oise" /></div>
+                <div id="planEauImage" style={{ display: 'none' }}><img className='image' src={img3} alt="Plan d'eau du Canada" /></div>
+                <div id="maladrerieImage" style={{ display: 'none' }}><img className='image' src={img4} alt="Maladrerie Saint-Lazare" /></div>
             </div>
         </div>
     );
