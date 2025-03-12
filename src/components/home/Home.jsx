437d3618b3e7a15d596df6f1d7c96655a24d4910
Guillaume Sere <guillaumesere@hotmail.fr>
2025-03-12 10:52:44 +0100

    modif home components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -27,20 +27,33 @@ const videoData = [
 const Home = () => {
 
     const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
+    const [nextVideoIndex, setNextVideoIndex] = useState(1)
 
     useEffect(() => {
       const interval = setInterval(() => {
         setCurrentVideoIndex((prevIndex) => 
           prevIndex === videoData.length - 1 ? 0 : prevIndex + 1
         )
+        setNextVideoIndex((prevIndex) => 
+          prevIndex === videoData.length - 1 ? 0 : prevIndex + 1
+        )
       }, 9000) // Change toutes les 10 secondes
   
       return () => clearInterval(interval)
     }, [])
-  
+
+    useEffect(() => {
+      const videoElement = document.querySelector(`#video-${nextVideoIndex}`);
+      if (videoElement) {
+        videoElement.load();
+        videoElement.play();
+      }
+    }, [nextVideoIndex]);
+
     return (
       <section className="video-section">
         <video
+          id={`video-${currentVideoIndex}`}
           key={videoData[currentVideoIndex].id}
           className="video-player"
           autoPlay
@@ -49,13 +62,29 @@ const Home = () => {
           playsInline
           preload="metadata"
           aria-label={videoData[currentVideoIndex].title}
+          onEnded={() => {
+            setCurrentVideoIndex(nextVideoIndex);
+          }}
         >
           <source src={process.env.PUBLIC_URL + videoData[currentVideoIndex].src} type="video/mp4" />
           Votre navigateur ne supporte pas la lecture de vidéos.
         </video>
+        <video
+          id={`video-${nextVideoIndex}`}
+          key={videoData[nextVideoIndex].id}
+          className="video-player"
+          muted
+          loop={false}
+          playsInline
+          preload="auto"
+          aria-label={videoData[nextVideoIndex].title}
+          style={{ display: 'none' }}
+        >
+          <source src={process.env.PUBLIC_URL + videoData[nextVideoIndex].src} type="video/mp4" />
+        </video>
         <div className="overlay-container">
           {/* Overlay optionnel pour améliorer la lisibilité du texte si nécessaire */}
-          Bienvenue à Beauvais
+         <h1>Bienvenue à Beauvais</h1> 
         </div>
       </section>
     )
