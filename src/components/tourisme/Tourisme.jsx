060aa7078b3a78ffcdcf604bd02ba176010bfb3b
Guillaume Sere <guillaumesere@hotmail.fr>
2025-03-12 16:17:57 +0100

    add animation

--- src/components/tourisme/Tourisme.jsx
+++ src/components/tourisme/Tourisme.jsx
@@ -7,16 +7,54 @@ import img3 from "../../images/tour1.png";
 import img4 from "../../images/maladrerie.png";
 
 const Tourisme = () => {
+    const handleMouseEnter = (id) => {
+        const element = document.getElementById(id);
+        element.style.display = 'block';
+        // Petit délai pour permettre à la transition de s'exécuter
+        setTimeout(() => element.style.opacity = '1', 0);
+    };
+
+    const handleMouseLeave = (id) => {
+        const element = document.getElementById(id);
+        element.style.opacity = '0';
+        // Attendre la fin de la transition avant de cacher l'élément
+        setTimeout(() => element.style.display = 'none', 200);
+    };
+
     return (
         <div className="tourisme">
             <h2>Tourisme à Beauvais</h2>
             <p>Découvrez les attractions touristiques de notre belle ville.</p>
             <div className="glass-card">
                 <ul>
-                    <li><Link to="/cathedrale" onMouseEnter={() => document.getElementById('cathedraleImage').style.display = 'block'} onMouseLeave={() => document.getElementById('cathedraleImage').style.display = 'none'}>La Cathédrale Saint-Pierre</Link></li>
-                    <li><Link to="/musee" onMouseEnter={() => document.getElementById('museeImage').style.display = 'block'} onMouseLeave={() => document.getElementById('museeImage').style.display = 'none'}>Le Musée départemental de l'Oise</Link></li>
-                    <li><Link to="/plan-eau" onMouseEnter={() => document.getElementById('planEauImage').style.display = 'block'} onMouseLeave={() => document.getElementById('planEauImage').style.display = 'none'}>Le Plan d'eau du Canada</Link></li>
-                    <li><Link to="/maladrerie" onMouseEnter={() => document.getElementById('maladrerieImage').style.display = 'block'} onMouseLeave={() => document.getElementById('maladrerieImage').style.display = 'none'}>La Maladrerie Saint-Lazare</Link></li>
+                    <li>
+                        <Link to="/cathedrale" 
+                            onMouseEnter={() => handleMouseEnter('cathedraleImage')} 
+                            onMouseLeave={() => handleMouseLeave('cathedraleImage')}>
+                            La Cathédrale Saint-Pierre
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/musee" 
+                            onMouseEnter={() => handleMouseEnter('museeImage')} 
+                            onMouseLeave={() => handleMouseLeave('museeImage')}>
+                            Le Musée départemental de l'Oise
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/plan-eau" 
+                            onMouseEnter={() => handleMouseEnter('planEauImage')} 
+                            onMouseLeave={() => handleMouseLeave('planEauImage')}>
+                            Le Plan d'eau du Canada
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/maladrerie" 
+                            onMouseEnter={() => handleMouseEnter('maladrerieImage')} 
+                            onMouseLeave={() => handleMouseLeave('maladrerieImage')}>
+                            La Maladrerie Saint-Lazare
+                        </Link>
+                    </li>
                 </ul>
                 <div id="cathedraleImage" style={{ display: 'none' }}><img className='image' src={img1} alt="Cathédrale Saint-Pierre" /></div>
                 <div id="museeImage" style={{ display: 'none' }}><img className='image' src={img2} alt="Musée départemental de l'Oise" /></div>
