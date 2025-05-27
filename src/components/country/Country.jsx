6c7c8184c8b0890122cba084072b01e688239357
Guillaume Sere <guillaumesere@hotmail.fr>
2025-05-27 15:25:15 +0200

    add images home page

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './country.css';
 import banner from "../../images/banner.png"
+import tatoo1 from "../../images/tattoo.1.png"
+import tatoo2 from "../../images/tattoo.2.png"
 import { data } from '../../data/data.js';
 import ScrollToTopButton from '../scrollToTopButton/ScollToTopButton';
 
@@ -8,10 +10,12 @@ const Country = () => {
     return (
         <>
             <div className='container'>
-            <img src={banner} alt="Bannière" className="responsive-banner" />
+                <img src={tatoo1} alt="tatoueurs" className='responsive-banner' />
+                <img src={banner} alt="Bannière" className="responsive-banner" />
                 <button className='btn'>
                     <a href="https://elispace.fr/programmation/bo-way-ink-tattoo-festival" target="_blank" rel="noopener noreferrer">Pour plus d’infos</a>
                 </button>
+                <img src={tatoo2} alt="tatoueurs" className='responsive-banner' />
             </div>
             <section className='country-section'>
                 <div className='box-container'>
