57cb6a70e030f46ae9a396c2be9bc0930e365f87
Guillaume Sere <guillaumesere@hotmail.fr>
2025-10-13 15:32:08 +0200

    add suspense to Home components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React, { Suspense } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
-import Home from "./components/home/Home";
 import Tourisme from "./components/tourisme/Tourisme";
 import Cathedrale from "./pages/cathedrale/Cathedrale";
 import Musee from "./pages/musee/Musee";
@@ -9,13 +9,18 @@ import Maladrerie from "./pages/maladrerie/Maladrerie";
 import Actualites from "./components/actualités/Actualites";
 import Footer from "./components/footer/Footer";
 import Sorties from "./components/sorties/Sorties";
+const Home = React.lazy(() => import('./components/home/Home'));
 
 function App() {
   return (
     <>
         <Header />
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={
+              <Suspense fallback={<div style={{padding: '2rem', textAlign: 'center'}}>Chargement...</div>}>
+                <Home />
+              </Suspense>
+            } />
             <Route path="/tourisme" element={<Tourisme />} />
             <Route path="/musee" element={<Musee />} />
             <Route path="/cathedrale" element={<Cathedrale />} />
