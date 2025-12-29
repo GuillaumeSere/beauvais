0965740e6ef575d0ac6a71f1a355e1e5e9f1ed8b
Guillaume Sere <guillaumesere@hotmail.fr>
2025-12-29 10:51:58 +0100

    modif

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -6,15 +6,12 @@ import tatoo2 from "../../images/tatoo2.png"
 import tatoo3 from "../../images/tatoo3.png"
 import tatoo4 from "../../images/tatoo4.png"
 import tatoo5 from "../../images/tatoo5.png"
-import tatoo6 from "../../images/tatoo6.png"
 import tatoo7 from "../../images/tatoo7.png"
-import tatoo8 from "../../images/tatoo8.png"
 import tatoo9 from "../../images/tatoo9.png"
 import tatoo10 from "../../images/tatoo10.png"
 import tatoo11 from "../../images/tatoo11.png"
 import tatoo12 from "../../images/tatoo12.png"
 import tatoo13 from "../../images/tatoo13.png"
-import tatoo14 from "../../images/tatoo14.png"
 import tatoo15 from "../../images/tatoo15.png"
 import tatoo16 from "../../images/tatoo16.png"
 import { data } from '../../data/data.js';
@@ -31,15 +28,12 @@ const Country = () => {
         tatoo3,
         tatoo4,
         tatoo5,
-        tatoo6,
         tatoo7,
-        tatoo8,
         tatoo9,
         tatoo10,
         tatoo11,
         tatoo12,
         tatoo13,
-        tatoo14,
         tatoo15,
         tatoo16
     ];
