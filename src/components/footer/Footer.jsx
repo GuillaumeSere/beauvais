e5cd82a3ee6f66399804538a095e44d7663d410d
Guillaume Sere <guillaumesere@hotmail.fr>
2026-01-12 10:50:47 +0100

    modif footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='footer'>
             <div className="footer-links">
                 <a href="https://www.linkedin.com/in/guillaume-séré" target="_blank" rel="noopener noreferrer"><BsLinkedin className='link' /></a>
-                <p className='footer-name'>&copy; Guillaume SERE</p>
+                <p className='footer-name'>&copy; {new Date().getFullYear()}  Guillaume SERE</p>
                 <a href="https://github.com/GuillaumeSere" target="_blank" rel="noopener noreferrer"><BsGithub className='link' /></a>
             </div>
         </div>
